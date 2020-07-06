d4dae44500b0ce573d8c577485eee20659a4a658
Roman <roma_klimovitskiy@mail.ru>
2020-07-06 11:01:44 +0300

    Added Apache Arrow linking, storing received data to arrow::Buffer

--- server.cpp
+++ server.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <memory>
 
 #include "server.h"
+
+#include <arrow/api.h>
 
 Server::Server(const std::shared_ptr<uvw::Loop>& loop) : tcp_(loop->resource<uvw::TCPHandle>()) {
   tcp_->on<uvw::ListenEvent>([](const uvw::ListenEvent& event, uvw::TCPHandle& server) {
     std::cerr << "New connection!" << std::endl;
 
     auto client = server.loop().resource<uvw::TCPHandle>();
+    client->data(std::make_shared<arrow::BufferBuilder>());
 
     client->on<uvw::DataEvent>([](const uvw::DataEvent& event, uvw::TCPHandle& client) {
-      std::cerr << "Data received: " << event.data << std::endl;
-      client.write(event.data.get(), event.length);
+      std::cerr << "Data received: " << event.data.get() << std::endl;
+      client.data<arrow::BufferBuilder>()->Append(event.data.get(), event.length);
+      std::shared_ptr<arrow::Buffer> buffer;
+      if (!client.data<arrow::BufferBuilder>()->Finish(&buffer).ok()) {
+        std::cerr << "Buffer constructing failed" << std::endl;
+        return;
+      }
+      client.write(reinterpret_cast<char *>(buffer->mutable_data()), buffer->size());
     });
 
     client->once<uvw::ErrorEvent>([](const uvw::ErrorEvent& event, uvw::TCPHandle& client) {
