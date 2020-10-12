cf9329bf4dfb5a2b57a2708f177d9632df447a14
Roman <roma_klimovitskiy@mail.ru>
2020-10-12 16:31:31 +0300

    Disabled clang-tidy linter

--- test_script.sh
+++ test_script.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Entering build directory: ${SCRIPT_DIR}/build ..."
 mkdir -p "${SCRIPT_DIR}/build"
 pushd "${SCRIPT_DIR}/build" || exit
-cmake .. -DENABLE_TESTS=ON -DCLANG_TIDY_LINT=ON
+cmake .. -DENABLE_TESTS=ON -DCLANG_TIDY_LINT=OFF
 make test_main
 ctest --verbose
 popd || exit 1
